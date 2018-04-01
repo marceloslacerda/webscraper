11ecb899aa5b5d95f2c21c3fdaa76ee572ac3969
Marcelo Lacerda <marceloslacerda@gmail.com>
2018-04-01 17:36:08 -0300

    not sure why I'm updating this lol

--- webscraper.py
+++ webscraper.py
@@ -7,7 +7,7 @@ import sys
 from io import StringIO
 from os.path import join
 from urllib.parse import urljoin
-from typing import Dict, Any
+from typing import Dict, Any, AnyStr
 
 import requests
 from lxml.html import parse
@@ -18,7 +18,7 @@ class PageError(ValueError):
         self.pname = pname
 
 
-def process_page(meta_data: Dict[str, Any], tree):
+def process_page(meta_data: Dict[AnyStr, Any], tree):
     actual_result = tree.xpath(meta_data['xpath_test_query'])
     expected_result = meta_data['xpath_test_result']
     if actual_result != expected_result:
@@ -29,14 +29,14 @@ def process_page(meta_data: Dict[str, Any], tree):
             meta_data['next_page_expected'])
 
 
-def get_tree(url: str, name: str):
+def get_tree(url: AnyStr, name: AnyStr):
     text = requests.get(url, auth=('Thumb', 'Scraper')).text
     with open(join('debug', name), 'w') as f:
         f.write(text)
     return parse(StringIO(text))
 
 
-def scrape_text(text_input: str):
+def scrape_text(text_input: AnyStr):
     pages = json.loads(text_input)
     base = 'https://yolaw-tokeep-hiring-env.herokuapp.com/'
     next_url = '/'
